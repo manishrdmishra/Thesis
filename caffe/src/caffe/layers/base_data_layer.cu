770a294baa5db8c4a1abbd1ad84da4c00294323e
manish mishra <manish.mishra@tum.de>
2016-06-16 14:42:12 +0200

    added forward_gpu for dense image data layer

--- caffe/src/caffe/layers/base_data_layer.cu
+++ caffe/src/caffe/layers/base_data_layer.cu
@@ -53,6 +53,29 @@ void BasePrefetchingDataLayer<Dtype>::Forward_gpu(
   prefetch_free_.push(batch);
 }
 
+
+template <typename Dtype>
+void BasePrefetchingDenseDataLayer<Dtype>::Forward_gpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  // First, join the thread
+  JoinPrefetchThread();
+  // Reshape to loaded data.
+  top[0]->ReshapeLike(this->prefetch_data_);
+  // Copy the data
+  caffe_copy(prefetch_data_.count(), prefetch_data_.cpu_data(),
+      top[0]->mutable_gpu_data());
+  if (this->output_labels_) {
+    // Reshape to loaded labels.
+    top[1]->ReshapeLike(prefetch_label_);
+    // Copy the labels.
+    caffe_copy(prefetch_label_.count(), prefetch_label_.cpu_data(),
+        top[1]->mutable_gpu_data());
+  }
+  // Start a new prefetch thread
+  CreatePrefetchThread();
+}
+
+INSTANTIATE_LAYER_GPU_FORWARD(BasePrefetchingDenseDataLayer);
 INSTANTIATE_LAYER_GPU_FORWARD(BasePrefetchingDataLayer);
 
 }  // namespace caffe
